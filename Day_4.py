6256006f77b9e80aeb32d85615d3396975469f4a
Dikshita Choudhary <dikshitachoudhary845@gmail.com>
2026-06-28 21:55:07 +0530

    Update Day_4.py

--- Day_4.py
+++ Day_4.py
@@ -1,1 +1,8 @@
+'''Design a class MyClass with a public function display() that prints "Hello World". The function takes no parameters and returns void.'''
+class MyClass:
+    def display(self):
+        print("Hello World")
 
+
+obj = MyClass()
+obj.display()
